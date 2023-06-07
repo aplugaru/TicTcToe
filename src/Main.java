69b6d96f2882379605458bc0bbe3f7133753e5ba
andreea p <andreea_20av@yahoo.com>
2023-06-07 16:50:22 +0300

    Other Version

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,69 @@ public class Main {
     }
 
     public static void main(String[] args) {
+// 1 version
+        //char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+        //{'-', '+', '-', '+', '-',},
+        //{' ', '|', ' ', '|', ' '},
+        //{'-', '+', '-', '+', '-'},
+        //{' ', '|', ' ', '|', ' '}};
 
-        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-',},
-                {' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '}};
+        //printGameBoard ( gameBoard );
 
-        printGameBoard ( gameBoard );
+        //Scanner scan = new Scanner (System.in);
+        //System.out.println ("Enter your placement (1-9): ");
+        //int pos = scan.nextInt ();
 
-        Scanner scan = new Scanner (System.in);
-        System.out.println ("Enter your placement (1-9): ");
-        int pos = scan.nextInt ();
+        //System.out.println (pos);
 
-        System.out.println (pos);
+        // 2 version
+
+        char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+        Scanner scanner = new Scanner ( System.in );
+        int input;
+        char turn = 'X';
+        int currentTurn = 1;
+
+
+        while (true) {
+            do {
+                System.out.print ( "Enter a position: " );
+                input = scanner.nextInt ();
+            } while (pos[input - 1] == 'X' || pos[input - 1] == 'O');
+            pos[input - 1] = turn;
+
+            System.out.println ( " " + pos[6] + " | " + pos[7] + " | " + pos[8] + " " );
+            System.out.println ( "---+---+---" );
+            System.out.println ( " " + pos[3] + " | " + pos[4] + " | " + pos[5] + " " );
+            System.out.println ( "---+---+---" );
+            System.out.println ( " " + pos[0] + " | " + pos[1] + " | " + pos[2] + " " );
+
+            if (pos[0] == turn && pos[1] == turn && pos[2] == turn
+                    ||pos[3] == turn && pos[4] == turn && pos[5] == turn
+                    ||pos[6] == turn && pos[7] == turn && pos[8] == turn
+                    ||pos[6] == turn && pos[3] == turn && pos[0] == turn
+                    ||pos[7] == turn && pos[4] == turn && pos[1] == turn
+                    ||pos[8] == turn && pos[5] == turn && pos[2] == turn
+                    ||pos[6] == turn && pos[4] == turn && pos[2] == turn
+                    ||pos[8] == turn && pos[4] == turn && pos[0] == turn) {
+                System.out.println (turn + " is the Winner");
+                break;
+            }
+
+            if (turn == 'X') {
+                turn = 'O';
+            } else if (turn == 'O') {
+                turn = 'X';
+            }
+
+            currentTurn++;
+            if (currentTurn > 9) {
+                System.out.println ("DRAW");
+                break;
+            }
+
+        }
 
     }
-
 }
